fc4aceb3b9ab38666a886c6ac4fb8f94444f172e
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-09-09 22:02:16 +0100

    MVP

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+
+document.addEventListener("DOMContentLoaded", () => {
+    new Vue({
+        el: "#app",
+        data: {
+            todos: ["Do Shopping", "Call Family", "Cook Dinner"],
+            newItem: ""
+        },
+        methods: {
+            saveNewItem: function() {
+                this.todos.push(this.newItem);
+                this.newItem="";
+            }
+
+        }
+    })
+})
